bbe85ec7b656436f35bcdf1063031a0efb201a0d
Abdulaziz <sinfsinf74@gmail.com>
2023-01-20 17:17:17 +0500

    tarnslate str, str2 ,str3

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ let user2 = {
     name: prompt("Enter second user name") , 
     age: +prompt("Enter second user age" )
 }
-let str = "dan katta"
-let str2 = "bilan "
-let str3 = "ni yoshlari teng" 
+let str = "older than "
+let str2 = "and "
+let str3 = " are the same age" 
 if( user1.age > user2.age ){
     user1.name = user1.name +" "
-    result = user1.name + user2.name + str
+    result = str + user1.name + user2.name 
     alert( result )
 }else if(user1.age == user2.age ){
     user1.name = user1.name +"  "
@@ -19,6 +19,6 @@ if( user1.age > user2.age ){
     alert( result )
 }else{
     user2.name = user2.name +"  "
-    result = user2.name + user1.name + str
+    result = str +user2.name + user1.name 
     alert( result )
 }
